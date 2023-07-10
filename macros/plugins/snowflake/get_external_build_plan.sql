0b659bc86059e1141c838c0f45b0a794b14c185d
IWinnik <ireneusz.winnik@gmail.com>
2023-07-10 10:05:58 +0200

    Change build plan based on create schema perms

--- macros/plugins/snowflake/get_external_build_plan.sql
+++ macros/plugins/snowflake/get_external_build_plan.sql
@@ -1,7 +1,9 @@
 {% macro snowflake__get_external_build_plan(source_node) %}
 
     {% set build_plan = [] %}
-    
+    {%- set external = source_node.external -%}
+    {%- set create_schema_forbidden = external.create_schema_forbidden -%}
+
     {% set old_relation = adapter.get_relation(
         database = source_node.database,
         schema = source_node.schema,
@@ -13,27 +15,30 @@
     {% if source_node.external.get('snowpipe', none) is not none %}
     
         {% if create_or_replace %}
-            {% set build_plan = build_plan + [
-                dbt_external_tables.create_external_schema(source_node),
-                dbt_external_tables.snowflake_create_empty_table(source_node),
-                dbt_external_tables.snowflake_get_copy_sql(source_node, explicit_transaction=true),
-                dbt_external_tables.snowflake_create_snowpipe(source_node)
-            ] %}
+            {% set build_plan = build_plan + (
+                ([dbt_external_tables.create_external_schema(source_node)] if not create_schema_forbidden else [])
+                + [
+                    dbt_external_tables.snowflake_create_empty_table(source_node),
+                    dbt_external_tables.snowflake_get_copy_sql(source_node, explicit_transaction=true),
+                    dbt_external_tables.snowflake_create_snowpipe(source_node)
+                ]
+            ) %}
         {% else %}
-            {% set build_plan = build_plan + dbt_external_tables.snowflake_refresh_snowpipe(source_node) %}
+            {% set build_plan = build_plan + [dbt_external_tables.snowflake_refresh_snowpipe(source_node)] %}
         {% endif %}
+
             
     {% else %}
     
         {% if create_or_replace %}
-            {% set build_plan = build_plan + [
-                dbt_external_tables.create_external_schema(source_node),
-                dbt_external_tables.create_external_table(source_node)
-            ] %}
+            {% set build_plan = build_plan + (
+                ([dbt_external_tables.create_external_schema(source_node)] if not create_schema_forbidden else [])
+                + [dbt_external_tables.create_external_table(source_node)]
+            ) %}
         {% else %}
-            {% set build_plan = build_plan + dbt_external_tables.refresh_external_table(source_node) %}
+            {% set build_plan = build_plan + [dbt_external_tables.refresh_external_table(source_node)] %}
         {% endif %}
-        
+
     {% endif %}
 
     {% do return(build_plan) %}
